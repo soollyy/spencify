749f1eb72b97e16dc8dc4fdb9cdf3276c41dd045
soollyy <solianatesema1@gmail.com>
2023-03-22 16:37:25 +0000

    classes to expense and expense entity

--- src/main/kotlin/com/example/demo/expense/Expense.kt
+++ src/main/kotlin/com/example/demo/expense/Expense.kt
@@ -1,4 +1,11 @@
 package com.example.demo.expense
+import java.time.LocalDate
 
-class Expense {
-}
+data class Expense(
+        val id: Long? = null,
+        val amount: Double,
+        val description: String,
+        val category: String,
+        val image: String? = null,
+        val date: LocalDate
+)
--- src/main/kotlin/com/example/demo/expense/ExpenseEntity.kt
+++ src/main/kotlin/com/example/demo/expense/ExpenseEntity.kt
@@ -1,4 +1,21 @@
 package com.example.demo.expense
 
-class ExpenseEntity {
-}
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+import java.time.LocalDate
+
+@Entity
+@Table(name = "expenses")
+data class ExpenseEntity(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: Long = 0,
+        val amount: Double,
+        val description: String,
+        val category: String,
+        val image: String? = null,
+        val date: LocalDate = LocalDate.now()
+)
